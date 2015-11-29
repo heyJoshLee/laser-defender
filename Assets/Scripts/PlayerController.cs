6fc7a3d7b0156cc0615cc14361fcfcbf0a8ff9fb
jleewebdev <lee45276@gmail.com>
2015-11-29 18:24:05 +0800

    Using Vector.left / right in player movement instead of xyz values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,9 +13,11 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3(-speed * Time.deltaTime, 0, 0 );
+			transform.position += new Vector3.left * speed * Time.deltaTime
+			
+			
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3(speed * Time.deltaTime, 0, 0);
+			transform.position += new Vector3.right * speed * Time.deltaTime
 		}
 	}
 	
