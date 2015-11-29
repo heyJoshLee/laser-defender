84777a778b4addea610d4dfc5e3238c1c51d0cf1
jleewebdev <lee45276@gmail.com>
2015-11-29 18:40:34 +0800

    Added player movement restrictions

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,21 +4,35 @@ using System.Collections;
 public class PlayerController : MonoBehaviour {
 
 	public float speed = 15.0f;
+	
+	float xmin;
+	float xmax;
+	public float padding = 1f;
 
 	// Use this for initialization
 	void Start () {
-	
+		float distance = transform.position.z - Camera.main.transform.position.z;
+		Vector3 leftmost = Camera.main.ViewportToWorldPoint(new Vector3(0, 0, distance));
+		Vector3 rightmost = Camera.main.ViewportToWorldPoint(new Vector3(1, 0, distance));
+		xmin = leftmost.x + padding;
+		xmax = rightmost.x -padding;
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetKey(KeyCode.LeftArrow)) {
-			transform.position += new Vector3.left * speed * Time.deltaTime
+			transform.position += Vector3.left * speed * Time.deltaTime;
 			
 			
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
-			transform.position += new Vector3.right * speed * Time.deltaTime
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		
+		// Ristricts movement
+		float newX = Mathf.Clamp(transform.position.x, xmin, xmax);
+		
+		transform.position = new Vector3(newX, transform.position.y, transform.position.z);
 	}
 	
 	
